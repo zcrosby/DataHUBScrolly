a77c457156dedf4e867bfefed07a5f8187cab365
Zalyndria Crosby <zscrosby@gmail.com>
2014-05-14 10:35:43 -0400

    tableau charts were placed in tabs

--- js/scrolly.js
+++ js/scrolly.js
@@ -1,10 +1,15 @@
+/*Tab functionality*/
+$(document).ready(function ($) {
+  $('#tabs').tab();
+});
+/*END Tab functionality*/
 
 function addHomeBtn(){
   var theWindow = $(window);
   var windowTop;
   var addHMBtn = $('#divToAddHomeBtn');
   var divXPos = addHMBtn.position().top;
-  var HMBtn = document.getElementById('Home-Btn');
+  var HMBtn = document.getElementById('home-btn');
 
   theWindow.on('scroll', function(){
     divXPos = addHMBtn.position().top;
@@ -13,22 +18,13 @@ function addHomeBtn(){
     if(windowTop > divXPos){
       if(addHMBtn.children("#extra").length != 1){
         HMBtn.style.display="inline";
-        $('#Home-Btn').fadeIn(1000);
+        $('#home-btn').fadeIn(1000);
       }
     }else if(windowTop<divXPos){
       HMBtn.style.display="none";
     }
 
   });
-}
-
-
-function initNav(){
-  $("#nav li a").click(function(e){
-    e.preventDefault();
-    var self = $(this);
-    moveContent(self.attr("href"));
-  })
 }
 
 function moveContent(targ){
@@ -52,8 +48,13 @@ function moveContent(targ){
   }
 }
 
-
-
+function initNav(){
+  $("#nav li a").click(function(e){
+    e.preventDefault();
+    var self = $(this);
+    moveContent(self.attr("href"));
+  })
+}
 
 $(function(){
   addHomeBtn();
