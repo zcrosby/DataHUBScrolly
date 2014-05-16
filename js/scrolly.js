e279a676599a97532c0a69f2277a6c8562dbbb93
Zalyndria Crosby <zscrosby@gmail.com>
2014-05-16 11:39:47 -0400

    added new charts and sectors. updated js to jump to new sectors. updated id names

--- js/scrolly.js
+++ js/scrolly.js
@@ -5,71 +5,51 @@ $(document).ready(function ($) {
 /*END Tab functionality*/
 
 function addHomeBtn(){
-  var theWindow = $(window);
-  var windowTop;
-  var addHMBtn = $('#divToAddHomeBtn');
-  var divXPos = addHMBtn.position().top;
-  var HMBtn = document.getElementById('home-btn');
+    var theWindow = $(window);
+    var windowTop;
+    var addHMBtn = $('#divToAddHomeBtn');
+    var divXPos = addHMBtn.position().top;
+    var HMBtn = document.getElementById('home-btn');
 
-  theWindow.on('scroll', function(){
-    divXPos = addHMBtn.position().top;
-    windowTop = theWindow.scrollTop();
+    theWindow.on('scroll', function(){
+      divXPos = addHMBtn.position().top;
+      windowTop = theWindow.scrollTop();
 
-    if(windowTop > divXPos){
-      if(addHMBtn.children("#extra").length != 1){
-        HMBtn.style.display="inline";
-        $('#home-btn').fadeIn(1000);
+      if(windowTop > divXPos){
+        if(addHMBtn.children("#extra").length != 1){
+          HMBtn.style.display="inline";
+          $('#home-btn').fadeIn(1000);
+        }
+      }else if(windowTop<divXPos){
+        HMBtn.style.display="none";
       }
-    }else if(windowTop<divXPos){
-      HMBtn.style.display="none";
-    }
 
-  });
+    });
 }
 
 function moveContent(targ){
 
-  var x = targ;
-  var xPos = $(x).position().top;
-  var len = 30;
+    var x = targ;
+    var xPos = $(x).position().top;
+    var len = 30;
 
-  switch(x){
-    case "#education":
-      $(window).scrollTop(xPos);
-      console.log(xPos);
-    break;
-
-    case "#human-services":
+    if(x == "#education"){
+      $(window).scrollTop(xPos + 80);
+    }
+    else{
       $(window).scrollTop(xPos - len);
-    break;
-
-    case "#environment":
-      $(window).scrollTop(xPos - len);
-    break;
-
-    case "#arts-culture":
-      $(window).scrollTop(xPos - len);
-    break;
-
-    case "#economy":
-      $(window).scrollTop(xPos - len);
-    break;
-
-    case "#housing":
-      $(window).scrollTop(xPos - len);
-    break;
-  }
+    }
 }
 
 function initNav(){
-  $("#nav li a").click(function(e){
-    e.preventDefault();
-    var self = $(this);
-    moveContent(self.attr("href"));
-  })
+    $("#nav li a").click(function(e){
+      e.preventDefault();
+      var self = $(this);
+      moveContent(self.attr("href"));
+    })
 }
 
 $(function(){
-  addHomeBtn();
-  initNav();
+    addHomeBtn();
+    initNav();
 });
