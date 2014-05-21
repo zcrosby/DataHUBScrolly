1e6e0d7dd85f92a0534c482421f9d34030cacebc
Zalyndria Crosby <zscrosby@gmail.com>
2014-05-21 14:44:55 -0400

    updated parser for better data structure

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import xlrd
 import json
+import re
 
 def main():
 	raw_file = "xl_files/May_2104_Dashboard_Indicators.xlsx"
@@ -21,25 +22,31 @@ def parse(file):
 	#nrows is a method of the Sheets class. It returns the number of rows in a sheet.
 	num_of_rows = worksheet.nrows
 
-	#empty list to add rows to
-	items = []
+	#empty dict to hold indicators w/in a sector
+	indicator_dict = {}
 
 	#add each row value to a dict, append dict to list
 	for sheet in workbook.sheets():
-		for row in range(sheet.nrows):#row is type int
+		for row in range(sheet.nrows):#row is type int, only an index
 			indicator_list = []
-			for col in range(sheet.ncols):#col is also type int
+			for col in range(sheet.ncols):#col is also type int, only an index
 				cell = sheet.cell(row, col).value
 				indicator_list.append(cell)
-				#indicator["sector"] = col[1]
-			indicator_dict = {}
-			indicator_dict["indicator"] = indicator_list[0]
-			indicator_dict["sector"] = indicator_list[1]
-			indicator_dict["sub-section"] = indicator_list[2]
-			indicator_dict["url"] = indicator_list[3]
-			items.append(indicator_dict)
 
-	return json.dumps(items)
+			if indicator_list[1] not in indicator_dict:
+				indicator_dict[indicator_list[1]] = []
+
+			clean_ind_txt = re.sub(r"INDICATOR \#\d\:\s", "", indicator_list[0])
+			#sclean_ind_txt = re.sub(r"Ri", "RI", clean_ind_txt)
+			indicator = {
+				"indicator" : re.sub(r"Ri","RI", clean_ind_txt.title()),
+				"sub-section" : indicator_list[2].title(),
+				"url": indicator_list[3]
+			}
+			
+			indicator_dict[indicator_list[1]].append(indicator)
+
+	return json.dumps(indicator_dict)
 
 if __name__ == "__main__":
 	main()
